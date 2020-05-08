1daf5e25ab6545b887bebb5216713edba8fe1dcd
Shunichi <shunichi.sd@gmail.com>
2020-05-08 13:22:12 +0900

    First Setting

---
+++ src/application/MFMController.java
@@ -0,0 +1,24 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class MFMController {
+	@FXML TextArea log;
+	@FXML
+	private void quitAction() {
+		System.exit(0);
+	}
+	@FXML
+	private void openAction() {
+		
+	}
+	@FXML
+	private void saveAction() {
+		
+	}
+	@FXML
+	private void translate() {
+		
+	}
+}
