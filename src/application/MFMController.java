6f86390115c6660a57291ba675c0eee636ceae54
Shunichi <shunichi.sd@gmail.com>
2020-05-10 21:33:26 +0900

    commit May10

--- src/application/MFMController.java
+++ src/application/MFMController.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -25,11 +24,37 @@ import javafx.stage.FileChooser;
 public class MFMController {
 	String encode = null;
 	String sysEncode = null;
+	final String headText = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n" + 
+			"<quiz>\r\n" + 
+			"<!-- question: 0  -->\r\n" + 
+			"  <question type=\"category\">\r\n" + 
+			"    <category>\r\n" + 
+			"        <text>$course$/test2020 デフォルト</text>\r\n" + 
+			"\r\n" + 
+			"    </category>\r\n" + 
+			"  </question>\r\n" + 
+			"\r\n" + 
+			"<!-- question: 27868  -->\r\n" + 
+			"  <question type=\"cloze\">\r\n" + 
+			"    <name>\r\n" + 
+			"      <text>問題名はここ</text>\r\n" + 
+			"    </name>\r\n" + 
+			"    <questiontext format=\"html\">\r\n" + 
+			"      <text><![CDATA[";
+	final String tailText =" ]]></text>\r\n" + 
+			"    </questiontext>\r\n" + 
+			"    <generalfeedback format=\"html\">\r\n" + 
+			"      <text></text>\r\n" + 
+			"    </generalfeedback>\r\n" + 
+			"    <penalty>0.3333333</penalty>\r\n" + 
+			"    <hidden>0</hidden>\r\n" + 
+			"  </question>\r\n" + 
+			"\r\n" + 
+			"</quiz>";
 	@FXML
 	TextArea mainArea;
 	@FXML
-	TextArea dummyArea;
-
+	TextArea outputArea;
 	@FXML
 	private void quitAction() {
 		System.exit(0);
@@ -100,11 +125,8 @@ public class MFMController {
 	//
 	@FXML
 	private void clearAction() {
-		if (mainArea.getText() != null) {
+		if (!mainArea.getText().trim().isEmpty()) {
 			mainArea.clear();
-		}
-		if (dummyArea.getText() != null) {
-			dummyArea.clear();
 		}
 		return;
 	}
@@ -137,32 +159,61 @@ public class MFMController {
 
 	@FXML
 	private void translate() {
-		//"{\toi:***}" のリスト
-		//List<String> toiList = new ArrayList<String>();
-		// 選択肢のリスト
-		List<String> itemList = new ArrayList<String>();
-		// mainArea の文字列から{\toi}を探す。
+		//元テキスト
 		String str = mainArea.getText();
-		//以下の正規表現で{\toi：*}を探している。
-		//"\\{" は '{' 、\\ は'\' のエスケープ。次の"\\\t" は特殊文字'\'をエスケープした後で
-		//"\t" をエスケープするためにもう一つ'\'が必要。
-		String regex = "\\{\\\\toi:.+?}";
+		//処理済みテキスト
+		String strBody = "";
+		// 元のテキストを選択し<s:....>ごとに分ける
+		String[] array = str.split("<s:.+?>");
+		// 選択肢の設定
+		String regex = "<s:.+?>";
 		Pattern p = Pattern.compile(regex);
 		Matcher m = p.matcher(str);
-		while(m.find()) {
-			String s = m.group().replace("{\\toi:", "");
-			s=s.replace("}", "");
+		List<String> itemList = new ArrayList<String>();
+		while (m.find()) {
+			String s = m.group();
+			System.out.println(s);
+			s = s.replace("<s:", "");
+			s = s.replace(">", "");
 			itemList.add(s);
 		}
-		//dummy の追加
-		if(dummyArea.getText()!=null) {
-			String[] array = dummyArea.getText().split(",");
-			for(String s:array) {
-				itemList.add(s);
-			}
-		}
-		for(String s: itemList) {
-			System.out.println(s);
-		}
-	}
+		// array の要素ごとに処理
+		for (int i = 0; i < array.length; i++) {
+			//System.out.println("+------loop " + i + "  ---------");
+			String target = array[i];
+			String regexQ = "<q:.+?>";
+			Pattern q = Pattern.compile(regexQ);
+			Matcher mq = q.matcher(target);
+			while (mq.find()) {
+				String oldStr = mq.group();
+				//System.out.println(oldStr);
+				// 正解文字列のストリップ
+				String correct = oldStr.replace("<q:", "");
+				correct = correct.replace(">", "");
+				// 新規のストリングを作成
+				String headStr = "{1:MULTICHOICE:";
+				String[] itemArray = itemList.get(i).split(",");
+				for (int j = 0; j < itemArray.length; j++) {
+					if (itemArray[j].equals(correct)) {
+						itemArray[j] = "=" + itemArray[j];
+					}
+				}
+				String subStr = "";
+				for (String s : itemArray) {
+					s = "~" + s;
+					subStr += s;
+				}
+				subStr = subStr.replaceFirst("~", "");
+				//System.out.println(subStr);
+				String newStr = headStr + subStr + "}";
+				//System.out.println(newStr);
+				target = target.replace(oldStr, newStr);
+			} // end of while(mq.find()}
+			//System.out.println(target);
+			strBody += target;
+		} // end of for(int i=0; ...
+		outputArea.setText(strBody);
+		System.out.println(strBody);
+		
+	} // end of translate()
 }
